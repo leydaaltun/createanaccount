db09d1c58c6f2bfe8b0fc51fea3995d7a64daf8a
Leyda <48352107+leydaaltun@users.noreply.github.com>
2019-06-25 14:39:18 +0300

    changed css

--- dist/createAnAccount.9ad09f98.js
+++ dist/createAnAccount.9ad09f98.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56089" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61575" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -393,4 +393,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../Users/leyda/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/createAnAccount.9ad09f98.js.map
+//# sourceMappingURL=/createanaccount.9ad09f98.js.map
